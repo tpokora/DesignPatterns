a7249303073ee02497dbf6b6cba1e4f53a3e613f
tpokora <pokora.tomek@gmail.com>
2015-11-18 22:04:29 +0100

    Running options

--- src/main/java/designpatterns/NewDesignPatterns.java
+++ src/main/java/designpatterns/NewDesignPatterns.java
@@ -11,10 +11,27 @@ import designpatterns.strategy.StrategyDesignPattern;
  */
 public class NewDesignPatterns {
     public static void main(String[] args) throws IllegalAccessException {
-        System.out.println("Hello new design patterns!\n");
-        NullObjectDesignPattern.main(null);
-        DecoratorDesignPattern.main(null);
-        SingletonDesignPattern.main(null);
-        StrategyDesignPattern.main(null);
+
+        if (args.length != 0) {
+            if (args[0].equals("--null")) {
+                NullObjectDesignPattern.main(null);
+            } else if (args[0].equals("--decorator")) {
+                DecoratorDesignPattern.main(null);
+            } else if (args[0].equals("--singleton")) {
+                SingletonDesignPattern.main(null);
+            } else if (args[0].equals("--strategy")) {
+                StrategyDesignPattern.main(null);
+            } else {
+                System.out.println("Unknown design pattern.");
+            }
+        } else {
+            System.out.println("Hello new design patterns!\n");
+            System.out.println("Use parameters to see design pattern:");
+            System.out.println("--null\t\t - Null object design pattern");
+            System.out.println("--decorator\t - Decorator design pattern");
+            System.out.println("--singleton\t - Singleton design pattern");
+            System.out.println("--strategy\t - Strategy design pattern");
+        }
+
     }
 }
